727f42fbc4816502cff89f0a47c7760d67ca27c3
game2 <game222antonina@gmail.com>
2025-08-26 23:05:26 +0300

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,6 @@
 export default async function Home() {
-  // Определяем базовый URL автоматически
-  const baseUrl =
-    process.env.NEXT_PUBLIC_BASE_URL ||
-    (typeof window === "undefined"
-      ? `http://${process.env.VERCEL_URL || "localhost:3000"}`
-      : "");
-
-  const res = await fetch(`${baseUrl}/api/sheet`, { cache: "no-store" });
+  // Для внутренних API Next.js можно использовать относительный путь
+  const res = await fetch("/api/sheet", { cache: "no-store" });
   const { data } = await res.json();
 
   return (
