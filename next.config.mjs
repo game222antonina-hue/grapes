2920c9fff519d08b7e2ebca0df9658e705c5e603
game2 <game222antonina@gmail.com>
2025-08-28 21:27:25 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  // Если нужно явно указать папку исходников
-  srcDir: "src"
-};
+const nextConfig = {};
 
 export default nextConfig;
